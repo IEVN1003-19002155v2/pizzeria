86e64b656d6b012df2b3080d7183255c33031b69
Jonathan Cortes <israelcortesss10@gmail.com>
2024-11-12 17:46:50 -0600

    Pizzeria Revisado 12/11/2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { DetallePedidoComponent } from './detalle-pedido/detalle-pedido.component';
+import { VentasDiaComponent } from './ventas-dia/ventas-dia.component';
+import { CapturaPedidoComponent } from './captura-pedido/captura-pedido.component';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports:[DetallePedidoComponent, VentasDiaComponent, CapturaPedidoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
